03ddb8b10e3b06343ae6db8daaca5924d1c55557
Akira <19719195+devakira@users.noreply.github.com>
2018-09-23 17:58:08 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow, Tray, process, Menu} = require('electron');
+const path = require('path');
+
+let mainWindow, tray;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+        frame: false,
+        resizable: true,
+        backgroundColor: "#2f3136",
+        webPreferences: {
+            zoomFactor: 1
+        },
+        icon: path.join(__dirname, 'assets/logo.png')
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    //mainWindow.webContents.openDevTools({
+    //    mode:"undocked"
+    //});
+
+    /*tray = new Tray('assets/tray.png');
+    tray.setToolTip('Click to open Discord in a minimal window.');
+    const trayContextMenu = Menu.buildFromTemplate([
+        {type: "normal", label: `TaskbarCord v${app.getVersion()}`, enabled: false},
+        {type: "separator"},
+        {type: "normal", label: "Quit Application", role: "quit"}
+    ]);
+    tray.setContextMenu(trayContextMenu);*/
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
---
+++ renderer.js
@@ -0,0 +1,40 @@
+const remote = require('electron').remote;
+var rpc = require("discord-rpc");
+var bot = new rpc.Client({transport:"ipc"});
+
+$(".action.cls").click(function() {
+    remote.app.quit();
+});
+
+$(".action.max").click(function() {
+    var window = remote.getCurrentWindow();
+    if (window.isMaximized()) {
+        window.unmaximize();
+    } else {
+        window.maximize();
+    }
+});
+
+$(".action.min").click(function() {
+    remote.getCurrentWindow().minimize();
+});
+
+var codemirror = new CodeMirror($(".editor")[0], {
+    lineNumbers: true,
+    lineWrapping: true,
+    autofocus: true
+});
+
+bot.on("ready",function(){
+    bot.setActivity({
+        details: "Editing some code",
+        state: "Working on a project",
+        largeImageKey: "generic_file",
+        largeImageText: "Editing a file",
+        smallImageKey: "logo",
+        smallImageText: "Voxyl Editor"
+    }).catch((reason)=>console.error(reason));
+    bot.createLobby();
+});
+
+bot.login({clientId:"",clientSecret:""}).catch((reason)=>console.error(reason));
